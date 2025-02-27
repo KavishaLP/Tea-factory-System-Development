8863c09dfceccb80c79cc0a88be960f007ec102e
Kavisha Shehani <158761723+KavishaLP@users.noreply.github.com>
2025-02-27 23:03:39 +0530

    update Login Process 21

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -2,15 +2,13 @@
 
 import express from 'express';
 import { login, logout } from '../controllers/authControllers.js';
-import { forgotPassword,resetPassword, checkCode,sendAgain } from '../controllers/authControllers.js';
+import { forgotPassword, checkCode, sendAgain, updatePassword } from '../controllers/authControllers.js';
 const router = express.Router();
 
 router.post('/login', login);
 router.post('/forgot-password', forgotPassword);
 router.post('/check-code', checkCode);
 router.post('/send-again', sendAgain);
-
-
 router.post('/update-password', updatePassword);
 
 export default router;
