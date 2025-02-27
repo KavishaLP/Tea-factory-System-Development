0a20a3509aac69cf6012419a117bf7af834e12a4
Kavisha Shehani <158761723+KavishaLP@users.noreply.github.com>
2025-02-27 21:41:14 +0530

    update Login Process 12

--- backend/controllers/authControllers.js
+++ backend/controllers/authControllers.js
@@ -2,6 +2,7 @@ import sqldb from '../config/sqldb.js';
 import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken';
 import dotenv from 'dotenv';
+import nodemailer from 'nodemailer';
 
 dotenv.config();
 
