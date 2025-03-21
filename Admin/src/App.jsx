37bb74610b2dc691557acd483596e72fdc610563
Kavisha Shehani <158761723+KavishaLP@users.noreply.github.com>
2025-03-21 22:05:58 +0530

    update css of dashboard

--- Admin/src/App.jsx
+++ Admin/src/App.jsx
@@ -1,21 +1,17 @@
 import './App.css';
-
 import { Navigate, Route, Routes, useLocation } from 'react-router-dom';
 import DashboardAdmin from './Pages/DashboardAdmin/DashboardAdmin';
 import AdvanceUpdate from './Pages/AdvanceUpdate/AdvanceUpdate';
 import TeaSackUpdate from './Pages/Teasackupdate/teasack';
 import TeaPacketDistribution from './Pages/TeaPaketDistribution/TeaPaketDistribution';
-
 import Login from './Pages/AuthPages/LoginRegister/login';
 import ForgetPassword from './Pages/AuthPages/ForgetPassword/ForgetPassword';
 import CheckMail from './Pages/AuthPages/CheckMail/CheckMail';
 import ForgetPasswordSuccess from './Pages/AuthPages/ForgetPasswordSuccess/ForgetPasswordSuccess';
 import UpdateNewPassword from './Pages/AuthPages/UpdateNewPassword/UpdateNewPassword';
-
 import Footer from './Component/Footer/Footer';
 import Navbar from './Component/Navbar/Navbar2';
 import Sidebar from './Component/sidebar/sidebar2';
-
 
 function App() {
   const location = useLocation();
@@ -47,9 +43,9 @@ function App() {
             <Route path="/update-new-password" element={<UpdateNewPassword />} />
             
             <Route path="/dashboard-admin" element={<DashboardAdmin />} /> 
-            <Route path="/tea-sack-update" element={<TeaSackUpdate/>} />
-            <Route path="/advance-update" element={<AdvanceUpdate/>} />
-            <Route path="/tea-packet-distribution" element={<TeaPacketDistribution/>} />
+            <Route path="/tea-sack-update" element={<TeaSackUpdate />} />
+            <Route path="/advance-update" element={<AdvanceUpdate />} />
+            <Route path="/tea-packet-distribution" element={<TeaPacketDistribution />} />
           </Routes>
         </main>
       </div>
