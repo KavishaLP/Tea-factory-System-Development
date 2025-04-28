4e0c18f701619936a99445069690c0dd3954ef64
Kavisha Shehani <158761723+KavishaLP@users.noreply.github.com>
2025-04-29 03:03:41 +0530

    refactor: Correct API endpoint for fetching total tea weight in DashboardAdmin component

--- Admin/src/Pages/DashboardAdmin/DashboardAdmin.jsx
+++ Admin/src/Pages/DashboardAdmin/DashboardAdmin.jsx
@@ -39,7 +39,7 @@ const DashboardAdmin = () => {
     const fetchTeaWeight = async () => {
       try {
         const dateString = formatDate(selectedDate);
-        const res = await axios.get(`http://localhost:8081/api/admin/fetch-tea-weight?date=${dateString}`, { withCredentials: true });
+        const res = await axios.get(`http://localhost:8081/api/admin/fetch-total-tea-weight'?date=${dateString}`, { withCredentials: true });
         setTotalTeaWeight(res.data.totalWeight || 0);
       } catch (error) {
         console.error("Error fetching tea weight:", error);
